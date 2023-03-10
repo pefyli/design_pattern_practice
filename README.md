fcc49daac2c69756cd61cedfe8ed70626aa19630
Pefyli <pefy19950714@gmail.com>
2023-03-10 22:50:24 +0800

    add singleton pattern and test case

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 ## design_pattern_practice
 This repo is a practice project for the design patterns, written by Java.
+
+
+## Design Patterns
+Design patterns are typical solutions to commonly occurring problems in software design. They are like pre-made blueprints that you can customize to solve a recurring design problem in your code.
+
+
+### Singleton
+A singleton is a class that allows only a single instance of itself to be created and gives access to that created instance. It contains static variables that can accommodate unique and private instances of itself. It is used in scenarios when a user wants to restrict instantiation of a class to only one object.
---
+++ design_pattern_practice/SingletonTest.java
@@ -0,0 +1,35 @@
+package design_pattern_practice;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SingletonTest extends Thread {
+	static Set<Integer> code = new HashSet<>();
+	
+    public void run() {
+		ThreadSafeSingleton singleton = ThreadSafeSingleton.getInstance();
+		SingletonTest.code.add(singleton.hashCode());
+	}
+
+	@Test
+	public void threadSafeTest() throws InterruptedException {
+
+		Thread th1 = new SingletonTest();
+		Thread th2 = new SingletonTest();
+
+		//Race-condition in that the state when printing depends on 
+		//the execution speed of the two independent threads. 
+		//adding sleep(100) to make sure we get hashcode
+		th1.start();
+		sleep(1000);
+		
+		th2.start();
+		sleep(1000);
+		
+		//make sure we don't have duplicates in the hash code set.
+		Assert.assertEquals(code.size(), 1);
+		
+	}
+}
